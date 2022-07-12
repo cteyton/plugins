fb33d260ec0ed11551a211899a7ff2cc8910cc6f
cteyton <cedric.teyton@promyze.com>
2022-07-12 17:11:28 +0200

    Update file.js

--- file.js
+++ file.js
@@ -9,3 +9,16 @@ async getAllCraftRooms() {
 async getAllCraftWorkshopSessions(craftRoomId) {
     return await this.ThemisAPIService.get(`api/craft-workshops-session/${craftRoomId}`);
 }
+
+async getUsersSessionInformation() {
+    if (!this.craftWorkshopInformations) {
+        return;
+    }
+
+    const sessionId = this.craftWorkshopInformations._id;
+    try {
+        return await this.ThemisAPIService.get(`api/craft-workshop-session/goal-information/${sessionId}`);
+    } catch (error) {
+        this.ThemisAPIService.handleError(error);
+    }
+}
